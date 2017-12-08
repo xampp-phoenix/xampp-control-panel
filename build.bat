d409e9e89dc64d8918de7702d0af4658f87eb692
speadup <speadup@gmail.com>
2017-12-08 23:01:49 +0800

    fix icon

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 cscript /nologo build.js win32
 cgrc -c65001 xampp_control3.rc -foxampp_control3.res 
 brcc32 -c65001 xampp_control3Resource.rc -foxampp_control3.dres 
-dcc32 -q -w -$O- -E.  xampp_control3.dpr
+dcc32 -q -b -w -$O+ -E.  xampp_control3.dpr
 move xampp_control3.exe xampp-control32.exe
 
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 cscript /nologo build.js win64
 cgrc -c65001 xampp_control3.rc -foxampp_control3.res 
 brcc32 -c65001 xampp_control3Resource.rc -foxampp_control3.dres 
-dcc64 -q -w -$O- -E.  xampp_control3.dpr
+dcc64 -q -b -w -$O+ -E.  xampp_control3.dpr
 move xampp_control3.exe xampp-control64.exe
 
-del /q /s *.dcu *.dres dcc*.cfg
+del /q /s *.dcu *.dres *.res dcc*.cfg
 if not defined nopause pause
